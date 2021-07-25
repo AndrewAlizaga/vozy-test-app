65481fb8e5a90f1fcde4f4df0ecbd460d2df1c23
Andrew Alizaga <andrewatld@gmail.com>
2021-07-25 15:58:27 -0600

    ready for review

--- routes/router.js
+++ routes/router.js
@@ -6,11 +6,12 @@ const router = express.Router();
 const auth = require("./auth")
 const account = require("./account")
 const post = require("./post")
+const documentation = require("./documentation")
 
 router.use('/auth', auth)
 router.use('/account', account)
 router.use('/post', post)
-///router.use('/documentation', )
+router.use('/documentation', documentation)
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json())
 app.use('/api', router);
 
-app.get('/', (req, res)=> {return res.send("<h1>Bienvenido!</h1><br><h2>Puedes encontrar la documentación de uso en esta ruta /api/documentation </h2>")})
+app.get('/', (req, res)=> {return res.send("<h1>Hola!</h1><br><h2>Puedes encontrar la documentación de uso en esta ruta /api/documentation o cliquear acá para ir directamente a la documentación <a target='_blank' href='https://documenter.getpostman.com/view/3678249/TzsZs8gL'>https://documenter.getpostman.com/view/3678249/TzsZs8gL</a></h2>")})
 
 const server = app.listen(port, '0.0.0.0', (req, res) => {
     console.log('Alive & listening at port: '+port);
